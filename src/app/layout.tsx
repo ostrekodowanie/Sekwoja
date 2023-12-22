f7ba8731b05ad9d4b83dab455f87a574ee0a96be
Filip <filfer05@gmail.com>
2023-12-22 22:34:54 +0100

    layout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={urbanist.className}>
         <Header />
-        <main>{children}</main>
+        <main className="min-h-screen">{children}</main>
         <Footer />
       </body>
     </html>
